b6573b449cbcf44395e1c0905ea44a4ef513b858
gabrielqueeniegomes <queeniegomes93@gmail.com>
2025-11-25 21:16:45 -0500

    Practice classes

---
+++ src/main/java/com/practice/test/FastFood.java
@@ -0,0 +1,12 @@
+package com.practice.test;
+
+public class FastFood {
+	static int burgerCost = 10;
+	static int ticketCost = 3;
+	
+	public static void purchaseMeal() {
+		System.out.println("Burger = $" + burgerCost);
+		System.out.println("Ticket = $" + ticketCost);
+	}
+
+}
