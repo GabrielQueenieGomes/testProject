c6d9687ea818c31b55f12bcffd706b7f0920ccc5
gabrielqueeniegomes <queeniegomes93@gmail.com>
2025-11-25 21:47:44 -0500

    Practice packages

---
+++ src/main/java/com/basic/java/practice/Animal.java
@@ -0,0 +1,12 @@
+package com.basic.java.practice;
+
+public class Animal {
+public int a = 10;
+public int b = 20;
+
+public void animalCount() {
+	int totalResult = a + b;
+	System.out.println("How many animals in the zoo? " + totalResult);
+}
+
+}
---
+++ src/test/java/com/test/run/TestRun.java
@@ -0,0 +1,13 @@
+package com.test.run;
+
+import com.basic.java.practice.Animal;
+
+public class TestRun {
+
+	public static void main(String[] args) {
+		Animal obj = new Animal();
+		obj.animalCount();
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+	}
+}
