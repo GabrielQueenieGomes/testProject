3f56268d621b795a2a4f6a1f2ba62b97b271b266
gabrielqueeniegomes <queeniegomes93@gmail.com>
2025-11-30 13:53:38 -0500

    parent and child inheritance

---
+++ src/main/java/com/basic/java/practice/Child.java
@@ -0,0 +1,10 @@
+package com.basic.java.practice;
+
+public class Child extends Parent {
+
+	public static void main(String[] args) {
+		Child obj = new Child();
+		System.out.println(obj.a);
+		obj.getMoney();
+	}
+}
---
+++ src/main/java/com/basic/java/practice/Parent.java
@@ -0,0 +1,10 @@
+package com.basic.java.practice;
+
+public class Parent {
+int a = 10;
+
+public void getMoney() {
+	System.out.println("This is a void method");
+}
+
+}
