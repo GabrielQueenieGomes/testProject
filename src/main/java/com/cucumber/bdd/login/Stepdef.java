a9daa79f7001c2dc9706edcc50b5e7548f017d66
gabrielqueeniegomes <queeniegomes93@gmail.com>
2025-12-02 17:26:59 -0500

    Revised paqge object model

--- src/main/java/com/cucumber/bdd/login/Stepdef.java
+++ src/main/java/com/cucumber/bdd/login/Stepdef.java
@@ -4,13 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.basic.java.practice.PageObjectModel;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Stepdef {
 	WebDriver driver;
-	PageObjectModel pom;
+
+	
 	@Given("open chrome")
 	public void open_chrome() {
 		driver = new ChromeDriver();
@@ -19,15 +22,16 @@ public class Stepdef {
 	
 	@Given("go to app link")
 	public void go_to_app_link() {
-		driver.get(
-				"file:///C:/Users/Queenie/Documents/qa_online%20Banking%20monthly_yearly%20statement%20-bug%20fixed.html");
+
+		driver.navigate().to(
+				PageObjectModel.APPLink);
 	}
 
 	
 	
 	@When("click login button")
 	public void click_login_button() {
-		driver.findElement(By.xpath("//*[@type='submit']")).click();
+		driver.findElement(By.xpath(PageObjectModel.loginBtn)).click();
 		try {
 	        Thread.sleep(3000);
 	    } catch (InterruptedException e) {
@@ -39,7 +43,7 @@ public class Stepdef {
 	public void check_login_pass_or_failed() {
 			
 		try {
-			Boolean status = driver.findElement(By.xpath("//*[@id='logoutButton']")).isDisplayed();
+			Boolean status = driver.findElement(By.xpath(PageObjectModel.logOutButton)).isDisplayed();
 			System.out.println("Log out button there or not = " + status);
 		} catch (Exception e) {
 			
@@ -52,7 +56,7 @@ public class Stepdef {
 	@Then("login fails and no logout button")
 	public void login_fails_and_no_logout_button() {
 		try {
-			Boolean status = driver.findElement(By.xpath("//*[@id='logoutButton']")).isDisplayed();
+			Boolean status = driver.findElement(By.xpath(PageObjectModel.logOutButton)).isDisplayed();
 			System.out.println("Log out button there or not = " + status);
 		} catch (Exception e) {
 			
@@ -68,18 +72,18 @@ public void open_any_browser() {
 
 @Given("go to application")
 public void go_to_application() {
-	driver.get(
-			"file:///C:/Users/Queenie/Documents/qa_online%20Banking%20monthly_yearly%20statement%20-bug%20fixed.html");
+	driver.navigate().to(
+			PageObjectModel.APPLink);
 }
 
 @When("enter user {string}")
 public void enter_user(String user) {
-	driver.findElement(By.xpath("//*[@id='username']")).sendKeys(user);
+	driver.findElement(By.xpath(PageObjectModel.user)).sendKeys(user);
 }
 
 @When("enter pass {string}")
 public void enter_pass(String pass) {
-	driver.findElement(By.xpath("//*[@id='password']")).sendKeys(pass);
+	driver.findElement(By.xpath(PageObjectModel.password)).sendKeys(pass);
 }
 
 
