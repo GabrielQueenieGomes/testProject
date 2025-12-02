fb3302aa947a61a19c330c298e965ce90b1bf188
gabrielqueeniegomes <queeniegomes93@gmail.com>
2025-12-02 13:58:42 -0500

    multiple feature paths

--- src/main/java/com/basic/java/practice/PageObjectModel.java
+++ src/main/java/com/basic/java/practice/PageObjectModel.java
@@ -5,5 +5,11 @@ public static final String APPLink = "file:///C:/Users/Queenie/Documents/qa_onli
 public static final String user = "//*[@id='username']";
 public static final String password = "//*[@id='password']";
 public static final String loginBtn = "//*[@value='Login']";
-public static final String logOutButton = "//*[@id='logoutButton']"
+public static final String logOutButton = "//*[@id='logoutButton']";
+
+private String APPLink2 = "file:///C:/Users/Queenie/Documents/qa_online%20Banking%20monthly_yearly%20statement%20-bug%20fixed.html";
+private String user2 = "//*[@id='username']";
+private String password2 = "//*[@id='password']";
+private String loginBtn2 = "//*[@value='Login']";
+private String logOutButton2 = "//*[@id='logoutButton']";
 }
